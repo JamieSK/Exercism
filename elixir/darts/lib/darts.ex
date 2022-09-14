c2bf7322f7b5c11fe3bcd46891c268c9a07c9cf6
Jamie Klein <jamie.klein@multiverse.io>
2022-09-14 13:26:52 +0100

    change sqrt to appease dialyzer

--- elixir/darts/lib/darts.ex
+++ elixir/darts/lib/darts.ex
@@ -6,7 +6,7 @@ defmodule Darts do
   """
   @spec score(position) :: integer
   def score({x, y}) do
-    case (x ** 2 + y ** 2) ** 0.5 do
+    case :math.sqrt(x ** 2 + y ** 2) do
       d when d <= 1 -> 10
       d when d <= 5 -> 5
       d when d <= 10 -> 1
