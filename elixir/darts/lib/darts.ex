b33cb1266879484f794774ce521547e169e2915d
Jamie Klein <jamie.klein@multiverse.io>
2022-09-14 11:41:35 +0100

    elixir/darts solution

--- elixir/darts/lib/darts.ex
+++ elixir/darts/lib/darts.ex
@@ -6,5 +6,11 @@ defmodule Darts do
   """
   @spec score(position) :: integer
   def score({x, y}) do
+    case (x ** 2 + y ** 2) ** 0.5 do
+      d when d <= 1 -> 10
+      d when d <= 5 -> 5
+      d when d <= 10 -> 1
+      _ -> 0
+    end
   end
 end
