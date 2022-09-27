3f94a5b64eb4a26c7b278ac0ca23e61fcbf6fcf9
Jamie Klein <jamie.klein@multiverse.io>
2022-09-27 12:04:38 +0100

    elixir/raindrops solution

--- elixir/raindrops/lib/raindrops.ex
+++ elixir/raindrops/lib/raindrops.ex
@@ -10,5 +10,27 @@ defmodule Raindrops do
   """
   @spec convert(pos_integer) :: String.t()
   def convert(number) do
+    {number, ""}
+    |> maybe_pling()
+    |> maybe_plang()
+    |> maybe_plong()
+    |> maybe_digits()
+    |> elem(1)
+  end
+
+  defp maybe_pling({number, string}) do
+    {number, (if Integer.mod(number, 3) == 0, do: string <> "Pling", else: string)}
+  end
+
+  defp maybe_plang({number, string}) do
+    {number, (if Integer.mod(number, 5) == 0, do: string <> "Plang", else: string)}
+  end
+
+  defp maybe_plong({number, string}) do
+    {number, (if Integer.mod(number, 7) == 0, do: string <> "Plong", else: string)}
+  end
+
+  defp maybe_digits({number, string}) do
+    {number, (if string == "", do: Integer.to_string(number), else: string)}
   end
 end
