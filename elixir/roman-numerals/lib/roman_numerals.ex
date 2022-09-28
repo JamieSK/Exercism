fae42a43ad7410147e4e8cb2a7b439cd28993b29
Jamie Klein <jamie.klein@multiverse.io>
2022-09-28 13:14:00 +0100

    elixir/roman-numerals solution

--- elixir/roman-numerals/lib/roman_numerals.ex
+++ elixir/roman-numerals/lib/roman_numerals.ex
@@ -4,5 +4,27 @@ defmodule RomanNumerals do
   """
   @spec numeral(pos_integer) :: String.t()
   def numeral(number) do
+    [
+      {1000, "M"},
+      {900, "CM"},
+      {500, "D"},
+      {400, "CD"},
+      {100, "C"},
+      {90, "XC"},
+      {50, "L"},
+      {40, "XL"},
+      {10, "X"},
+      {9, "IX"},
+      {5, "V"},
+      {4, "IV"},
+      {1, "I"}
+    ]
+    |> Enum.reduce({"", number}, fn {value, numeral}, {numerals, remainder} ->
+      {
+        numerals <> String.duplicate(numeral, div(remainder, value)),
+        rem(remainder, value)
+      }
+    end)
+    |> elem(0)
   end
 end
