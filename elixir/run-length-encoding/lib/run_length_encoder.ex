86ff54fa333d4dd603e2f923e86422c05ac60112
Jamie Klein <jamie.klein@multiverse.io>
2022-09-29 16:34:20 +0100

    elixir/run-length-encoding solution

--- elixir/run-length-encoding/lib/run_length_encoder.ex
+++ elixir/run-length-encoding/lib/run_length_encoder.ex
@@ -8,9 +8,31 @@ defmodule RunLengthEncoder do
   """
   @spec encode(String.t()) :: String.t()
   def encode(string) do
+    string
+    |> String.to_charlist()
+    |> Enum.chunk_by(&(&1))
+    |> Enum.reduce("", fn chars, acc ->
+      acc <> (chars |> List.to_string() |> then(&(count(&1) <> String.first(&1))))
+    end)
   end
 
   @spec decode(String.t()) :: String.t()
   def decode(string) do
+    string
+    |> String.split(~r/\d*[^\d]{1}/, include_captures: true, trim: true)
+    |> Enum.reduce("", fn s, acc ->
+      acc <> case s do
+        <<char::binary-size(1)>> -> char
+        <<digit::binary-size(1), char::binary-size(1)>> -> String.duplicate(char, String.to_integer(digit))
+        <<digit::binary-size(2), char::binary-size(1)>> -> String.duplicate(char, String.to_integer(digit))
+      end
+    end)
+  end
+
+  defp count(string) do
+    case String.length(string) do
+      l when l == 1 -> ""
+      l -> Integer.to_string(l) 
+    end
   end
 end
