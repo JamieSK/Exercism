34f2b025397e3bf936589c0fc33d9c3fec1ccd57
Jamie Klein <jamie.klein@multiverse.io>
2022-11-16 11:44:36 +0000

    elixir/sublist solution

--- elixir/sublist/lib/sublist.ex
+++ elixir/sublist/lib/sublist.ex
@@ -3,6 +3,18 @@ defmodule Sublist do
   Returns whether the first list is a sublist or a superlist of the second list
   and if not whether it is equal or unequal to the second list.
   """
-  def compare(a, b) do
+  def compare(a, b) when a === b, do: :equal
+  def compare(_, []), do: :superlist
+  def compare([], _), do: :sublist
+  def compare(a, b) when length(b) < length(a) do
+    Enum.reduce_while(Enum.chunk_every(a, length(b), 1, :discard), :unequal, fn sublist, match_found ->
+      if sublist === b, do: {:halt, :superlist}, else: {:cont, match_found}
+    end)
   end
+  def compare(a, b) when length(a) < length(b) do
+    Enum.reduce_while(Enum.chunk_every(b, length(a), 1, :discard), :unequal, fn sublist, match_found ->
+      if sublist === a, do: {:halt, :sublist}, else: {:cont, match_found}
+    end)
+  end
+  def compare(_a, _b), do: :unequal
 end
--- elixir/sublist/test/sublist_test.exs
+++ elixir/sublist/test/sublist_test.exs
@@ -1,140 +1,113 @@
 defmodule SublistTest do
   use ExUnit.Case
 
-  # @tag :pending
   test "empty equals empty" do
     assert Sublist.compare([], []) == :equal
   end
 
-  @tag :pending
-  test "empty list within non empty list" do
+    test "empty list within non empty list" do
     assert Sublist.compare([], [1, 2, 3]) == :sublist
   end
 
-  @tag :pending
-  test "non empty list contains empty list" do
+    test "non empty list contains empty list" do
     assert Sublist.compare([1, 2, 3], []) == :superlist
   end
 
-  @tag :pending
-  test "list equals itself" do
+    test "list equals itself" do
     assert Sublist.compare([1, 2, 3], [1, 2, 3]) == :equal
   end
 
-  @tag :pending
-  test "different lists" do
+    test "different lists" do
     assert Sublist.compare([1, 2, 3], [2, 3, 4]) == :unequal
   end
 
-  @tag :pending
-  test "comparing massive equal lists" do
+    test "comparing massive equal lists" do
     l = Enum.to_list(1..1_000_000)
     assert Sublist.compare(l, l) == :equal
   end
 
-  @tag :pending
-  test "false start" do
+    test "false start" do
     assert Sublist.compare([1, 2, 5], [0, 1, 2, 3, 1, 2, 5, 6]) == :sublist
   end
 
-  @tag :pending
-  test "consecutive" do
+    test "consecutive" do
     assert Sublist.compare([1, 1, 2], [0, 1, 1, 1, 2, 1, 2]) == :sublist
   end
 
-  @tag :pending
-  test "sublists at start" do
+    test "sublists at start" do
     assert Sublist.compare([0, 1, 2], [0, 1, 2, 3, 4, 5]) == :sublist
   end
 
-  @tag :pending
-  test "sublists in middle" do
+    test "sublists in middle" do
     assert Sublist.compare([2, 3, 4], [0, 1, 2, 3, 4, 5]) == :sublist
   end
 
-  @tag :pending
-  test "sublists at end" do
+    test "sublists at end" do
     assert Sublist.compare([3, 4, 5], [0, 1, 2, 3, 4, 5]) == :sublist
   end
 
-  @tag :pending
-  test "partially matching sublist at end" do
+    test "partially matching sublist at end" do
     assert Sublist.compare([1, 1, 2], [1, 1, 1, 2]) == :sublist
   end
 
-  @tag :pending
-  test "sublist early in huge list" do
+    test "sublist early in huge list" do
     assert Sublist.compare([3, 4, 5], Enum.to_list(1..1_000_000)) == :sublist
   end
 
-  @tag :pending
-  @tag :slow
+    @tag :slow
   test "huge sublist not in huge list" do
     assert Sublist.compare(Enum.to_list(10..1_000_001), Enum.to_list(1..1_000_000)) == :unequal
   end
 
-  @tag :pending
-  test "at start of superlist" do
+    test "at start of superlist" do
     assert Sublist.compare([0, 1, 2, 3, 4, 5], [0, 1, 2]) == :superlist
   end
 
-  @tag :pending
-  test "in middle of superlist" do
+    test "in middle of superlist" do
     assert Sublist.compare([0, 1, 2, 3, 4, 5], [2, 3]) == :superlist
   end
 
-  @tag :pending
-  test "at end of superlist" do
+    test "at end of superlist" do
     assert Sublist.compare([0, 1, 2, 3, 4, 5], [3, 4, 5]) == :superlist
   end
 
-  @tag :pending
-  test "at end of partially matching superlist" do
+    test "at end of partially matching superlist" do
     assert Sublist.compare([1, 1, 1, 2], [1, 1, 2]) == :superlist
   end
 
-  @tag :pending
-  test "superlist early in huge list" do
+    test "superlist early in huge list" do
     assert Sublist.compare(Enum.to_list(1..1_000_000), [3, 4, 5]) == :superlist
   end
 
-  @tag :pending
-  test "first list missing element from second list" do
+    test "first list missing element from second list" do
     assert Sublist.compare([1, 3], [1, 2, 3]) == :unequal
   end
 
-  @tag :pending
-  test "second list missing element from first list" do
+    test "second list missing element from first list" do
     assert Sublist.compare([1, 2, 3], [1, 3]) == :unequal
   end
 
-  @tag :pending
-  test "first list missing additional digits from second list" do
+    test "first list missing additional digits from second list" do
     assert Sublist.compare([1, 2], [1, 22]) == :unequal
   end
 
-  @tag :pending
-  test "order matters to a list" do
+    test "order matters to a list" do
     assert Sublist.compare([1, 2, 3], [3, 2, 1]) == :unequal
   end
 
-  @tag :pending
-  test "same digits but different numbers" do
+    test "same digits but different numbers" do
     assert Sublist.compare([1, 0, 1], [10, 1]) == :unequal
   end
 
-  @tag :pending
-  test "strict equality needed" do
+    test "strict equality needed" do
     assert Sublist.compare([1], [1.0, 2]) == :unequal
   end
 
-  @tag :pending
-  test "recurring values sublist" do
+    test "recurring values sublist" do
     assert Sublist.compare([1, 2, 1, 2, 3], [1, 2, 3, 1, 2, 1, 2, 3, 2, 1]) == :sublist
   end
 
-  @tag :pending
-  test "recurring values unequal" do
+    test "recurring values unequal" do
     assert Sublist.compare([1, 2, 1, 2, 3], [1, 2, 3, 1, 2, 3, 2, 3, 2, 1]) == :unequal
   end
 end
